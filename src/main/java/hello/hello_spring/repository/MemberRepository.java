d888bc815d2b23df8eead280e0040db49f1b6920
Geurim <kimminje7810@gmail.com>
2025-05-25 17:47:24 +0900

    refactor: remove redundant save method in MemberRepository

--- src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Number number);
-
     Member save(Member member);
 
     Optional<Member> findById(Long id);
